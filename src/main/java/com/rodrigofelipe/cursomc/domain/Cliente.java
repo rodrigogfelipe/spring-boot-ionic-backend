d31dfa3134b7a51c93ccbb7d7ff99e78e5e7849d
Rodrigo Felipe <rodrigogfelipe@hotmail.com>
2019-10-31 17:40:16 -0300

    PUT, DELETE E GET para cliente

--- src/main/java/com/rodrigofelipe/cursomc/domain/Cliente.java
+++ src/main/java/com/rodrigofelipe/cursomc/domain/Cliente.java
@@ -23,7 +23,7 @@ import com.rodrigofelipe.cursomc.domain.enums.TipoCliente;
 public class Cliente implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
@@ -35,7 +35,7 @@ public class Cliente implements Serializable {
 	private Integer tipo;
 
 	/* Declarando lista do obj da Classe Endereco */
-	
+
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 
@@ -55,113 +55,91 @@ public class Cliente implements Serializable {
 	}
 
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
-
 		super();
-
 		this.id = id;
-
 		this.nome = nome;
-
 		this.email = email;
-
 		this.cpfOuCnpj = cpfOuCnpj;
-
-		this.tipo = tipo.getCod();
+		this.tipo = (tipo == null) ? null : tipo.getCod();
 
 	}
 
 	public Integer getId() {
-
 		return id;
 
 	}
 
 	public void setId(Integer id) {
-
 		this.id = id;
 
 	}
 
 	public String getNome() {
-
 		return nome;
 
 	}
 
 	public void setNome(String nome) {
-
 		this.nome = nome;
 
 	}
 
 	public String getEmail() {
-
 		return email;
 
 	}
 
 	public void setEmail(String email) {
-
 		this.email = email;
 
 	}
 
 	public String getCpfOuCnpj() {
-
 		return cpfOuCnpj;
 
 	}
 
 	public void setCpfOuCnpj(String cpfOuCnpj) {
-
 		this.cpfOuCnpj = cpfOuCnpj;
 
 	}
 
 	public TipoCliente getTipo() {
-
 		return TipoCliente.toEnum(tipo);
 
 	}
 
 	public void setTipo(TipoCliente tipo) {
-
 		this.tipo = tipo.getCod();
 
 	}
 
 	public List<Endereco> getEnderecos() {
-
 		return enderecos;
 
 	}
 
 	public void setEnderecos(List<Endereco> enderecos) {
-
 		this.enderecos = enderecos;
 
 	}
 
 	public Set<String> getTelefones() {
-
 		return telefones;
 
 	}
 
 	public void setTelefones(Set<String> telefones) {
-
 		this.telefones = telefones;
 
 	}
 
 	public List<Pedido> getPedidos() {
-
 		return pedidos;
 
 	}
 
 	public void setPedidos(List<Pedido> pedidos) {
-
 		this.pedidos = pedidos;
 
 	}
--- src/main/java/com/rodrigofelipe/cursomc/services/ClienteService.java
+++ src/main/java/com/rodrigofelipe/cursomc/services/ClienteService.java
@@ -3,10 +3,16 @@ package com.rodrigofelipe.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.rodrigofelipe.cursomc.domain.Cliente;
+import com.rodrigofelipe.cursomc.dto.ClienteDTO;
 import com.rodrigofelipe.cursomc.repositories.ClienteRepository;
+import com.rodrigofelipe.cursomc.services.exceptions.DataIntegrityException;
 import com.rodrigofelipe.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -22,4 +28,61 @@ public class ClienteService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
 
+	/*
+	 * metado Cliente para inserir no BD. ID for NULL e inserido, caso ao contrario
+	 * não inserir
+	 */
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+
+	/* metado Cliente para UPDATE no BD */
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+
+	}
+
+	/* metado Cliente para DELETE no BD */
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+
+		}
+
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+
+		}
+
+	}
+
+	/* metado para listar todas as Cliente BD */
+	public java.util.List<Cliente> findAll() {
+		return (java.util.List<Cliente>) repo.findAll();
+
+	}
+
+	/*
+	 * Metado Page<Cliente> findPage ordenas todos os dados no BD, mostrando os
+	 * números de elementos da página
+	 */
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+
+	}
+
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+
+	}
 }
