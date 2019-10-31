0fda96d8c479629c94fe8c6bc2b523d076354762
Rodrigo Felipe <rodrigogfelipe@hotmail.com>
2019-10-31 14:18:32 -0300

    Personalizando o retorno da validação

--- src/main/java/com/rodrigofelipe/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/rodrigofelipe/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -5,8 +5,11 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
 import com.rodrigofelipe.cursomc.services.exceptions.ObjectNotFoundException;
 
 /* Classe ResourceExceptionHandler*/
@@ -26,6 +29,18 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),
 				System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	/*ResponseEntity<StandardError> exception na validação do campos */
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação",
+				System.currentTimeMillis());
+		for (FieldError x : e.getBindingResult().getFieldErrors()) {
+			err.addError(x.getField(), x.getDefaultMessage());
+		}
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+
 	}
 
 }
