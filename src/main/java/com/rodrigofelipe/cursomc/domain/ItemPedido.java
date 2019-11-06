1d05414880dc9772c8a9c80e2c5eadeb4eddb650
Rodrigo Felipe <rodrigogfelipe@hotmail.com>
2019-11-06 16:47:27 -0300

    Configuração e envio do projeto para produção

--- src/main/java/com/rodrigofelipe/cursomc/domain/ItemPedido.java
+++ src/main/java/com/rodrigofelipe/cursomc/domain/ItemPedido.java
@@ -1,6 +1,8 @@
 package com.rodrigofelipe.cursomc.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
@@ -9,20 +11,20 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class ItemPedido implements Serializable {
-	private static final long serialVersionUID = 1L; 
-	
+	private static final long serialVersionUID = 1L;
+
 	/* Gerando ItemPedidoPK ID */
 	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
-	
+
 	private Double desconto;
 	private Integer quantidade;
 	private Double preco;
-	
-	/*Declarando o construtor padrao*/
+
+	/* Declarando o construtor padrao */
 	public ItemPedido() {
-		
+
 	}
 
 	public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, Double preco) {
@@ -34,30 +36,31 @@ public class ItemPedido implements Serializable {
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
-	
-	/* Metado getSubTotal para calcular Subtotal dos pedidos*/
+
+	/* Metado getSubTotal para calcular Subtotal dos pedidos */
 	public double getSubTotal() {
 		return (preco - desconto) * quantidade;
 	}
-	
-	/*Declarando GET Pedido/ Produto para ter acessos fora da Classe*/
+
+	/* Declarando GET Pedido/ Produto para ter acessos fora da Classe */
 	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
+
 	public void setPedido(Pedido pedido) {
 		id.setPedido(pedido);
 	}
-	
+
 	public Produto getProduto() {
 		return id.getProduto();
 	}
-	
+
 	public void setProduto(Produto produto) {
 		id.setProduto(produto);
 	}
-	
-	/*GETTER AND SETTER*/
+
+	/* GETTER AND SETTER */
 	public ItemPedidoPK getId() {
 		return id;
 	}
@@ -114,15 +117,21 @@ public class ItemPedido implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	@Override
+	public String toString() {
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR")); /*NumberFormat formato (dinheiro)*/
+		StringBuilder builder = new StringBuilder();
+		builder.append(getProduto().getNome()); /* Imprimir o nome do produto */
+		builder.append(", Qte: ");
+		builder.append(getQuantidade());
+		builder.append(", Preço unitário: ");
+		builder.append(nf.format(getPreco()));
+		builder.append(", Subtotal: ");
+		builder.append(nf.format(getSubTotal()));
+		builder.append("\n");
+		return builder.toString();
+
+	}
 
 }
--- src/main/java/com/rodrigofelipe/cursomc/services/PedidoService.java
+++ src/main/java/com/rodrigofelipe/cursomc/services/PedidoService.java
@@ -3,6 +3,8 @@ package com.rodrigofelipe.cursomc.services;
 import java.util.Date;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,7 @@ import com.rodrigofelipe.cursomc.domain.ItemPedido;
 import com.rodrigofelipe.cursomc.domain.PagamentoComBoleto;
 import com.rodrigofelipe.cursomc.domain.Pedido;
 import com.rodrigofelipe.cursomc.domain.enums.EstadoPagamento;
+import com.rodrigofelipe.cursomc.repositories.ClienteRepository;
 import com.rodrigofelipe.cursomc.repositories.ItemPedidoRepository;
 import com.rodrigofelipe.cursomc.repositories.PagamentoRepository;
 import com.rodrigofelipe.cursomc.repositories.PedidoRepository;
@@ -33,18 +36,25 @@ public class PedidoService {
 	@Autowired
 	private ProdutoService produtoService;
 
+	@Autowired
+	ClienteService clienteService;
+
 	/* SE obj ID for encontrado retorna ID, SE NAO return ObjectNotFoundException */
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 	}
-	/*pedido: um novo pedido a ser inserido na base de dados */
+
+	/* pedido: um novo pedido a ser inserido na base de dados */
+	@Transactional
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
+
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
 			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
 			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
@@ -53,14 +63,17 @@ public class PedidoService {
 
 		obj = repo.save(obj);
 		pagamentoRepository.save(obj.getPagamento());
+
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 
 		}
 
 		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
 
 	}
