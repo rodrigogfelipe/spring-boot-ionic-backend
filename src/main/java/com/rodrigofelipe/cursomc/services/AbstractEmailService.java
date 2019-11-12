aeb52429d3f67ff7e1d3f388b29e274b75efb01e
Rodrigo Felipe <rodrigogfelipe@hotmail.com>
2019-11-12 14:26:58 -0300

    email-html

--- src/main/java/com/rodrigofelipe/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/rodrigofelipe/cursomc/services/AbstractEmailService.java
@@ -2,12 +2,26 @@ package com.rodrigofelipe.cursomc.services;
 
 import java.util.Date;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import com.rodrigofelipe.cursomc.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService {
+
+	@Autowired
+	private TemplateEngine templateEngine;
+
+	@Autowired
+	private JavaMailSender javaMailSender;
 
 	/*
 	 * ("${default.sender}") e o endereço do email salvo na configuração
@@ -22,24 +36,48 @@ public abstract class AbstractEmailService implements EmailService {
 		SimpleMailMessage sm = prepareSimpleMailMessageFromPedido(obj);
 		sendEmail(sm);
 	}
-	/* sm.setTo e o destinario do email */
-	/*
-	 * setFrom(sender) e o endereço do email salvo na configuração
-	 * appllication.propeties
-	 */
-	/* setSubject e o assunto do email */
-	/* setSentDate tem a função de criar a data com horario do servidor */
-	/* setText tem a função do corpo do email */
 
 	protected SimpleMailMessage prepareSimpleMailMessageFromPedido(Pedido obj) {
 		SimpleMailMessage sm = new SimpleMailMessage();
-		sm.setTo(obj.getCliente().getEmail());
-		sm.setFrom(sender);
-		sm.setSubject("Pedido confirmado! Código: " + obj.getId());
-		sm.setSentDate(new Date(System.currentTimeMillis()));
-		sm.setText(obj.toString());
+		sm.setTo(obj.getCliente().getEmail()); /* sm.setTo e o destinario do email */
+		sm.setFrom(sender); /* setFrom(sender) e o endereço do email salvo na configuração appllication.propeties*/
+		sm.setSubject("Pedido confirmado! Código: " + obj.getId()); /* setSubject e o assunto do email */
+		sm.setSentDate(new Date( System.currentTimeMillis())); /* setSentDate tem a função de criar a data com horario do servidor */
+		sm.setText(obj.toString()); /* setText tem a função do corpo do email */
 		return sm;
 
+	}
+
+	/* htmlFromTemplatePedido será responsável por retornar o HTML preenchido com os dados de um pedido, a partir do template Thymeleaf */
+	protected String htmlFromTemplatePedido(Pedido obj) {
+		Context context = new Context();
+		context.setVariable("pedido", obj);
+		return templateEngine.process("email/confirmacaoPedido", context);
+
+	}
+
+	@Override
+	public void sendOrderConfirmationHtmlEmail(Pedido obj) {
+		try {
+			MimeMessage mm = prepareMimeMessageFromPedido(obj);
+			sendHtmlEmail(mm);
+			
+		} catch (MessagingException e) {
+			sendOrderConfirmationEmail(obj);
+		}
+
+	}
+
+	private MimeMessage prepareMimeMessageFromPedido(Pedido obj) throws MessagingException {
+		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+		MimeMessageHelper mmh = new MimeMessageHelper(mimeMessage, true);
+		mmh.setTo(obj.getCliente().getEmail());
+		mmh.setFrom(sender);
+		mmh.setSubject("Pedido confirmado! Código: " + obj.getId());
+		mmh.setSentDate(new Date(
+				System.currentTimeMillis())); /* setSentDate tem a função de criar a data com horario do servidor */
+		mmh.setText(htmlFromTemplatePedido(obj), true); /* setText tem a função do corpo do email */
+		return mimeMessage;
 	}
 
 }
--- src/main/java/com/rodrigofelipe/cursomc/services/EmailService.java
+++ src/main/java/com/rodrigofelipe/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.rodrigofelipe.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.rodrigofelipe.cursomc.domain.Pedido;
@@ -10,4 +12,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	/*sendEmail tenha a função de enviar o email */
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj); 
+	/*MimeMessage enviar email em HTML*/
+	void sendHtmlEmail(MimeMessage msg); 
 }
